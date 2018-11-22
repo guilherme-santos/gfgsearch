8871adb1934f6c5de9cff844cdb9d9fe15814811
Guilherme Santos <guilherme.santos@foodora.com>
2018-11-22 22:17:00 +0100

    Add basic datatypes to represent business model

---
+++ gfgsearch.go
@@ -0,0 +1,31 @@
+package gfgsearch
+
+import "context"
+
+// Product contains all the information about a specific product.
+type Product struct {
+	Title string `json:"title"`
+	Brand string `json:"brand"`
+	Price int32  `json:"price"`
+	Stock int32  `json:"stock"`
+}
+
+// Options contains some properties that can be available or not in
+// the request and it'll be used during the search phase.
+type Options struct {
+	Page    int32
+	PerPage int32
+	// Filter is a map that contains the field name and the text to filter.
+	Filter map[string]string
+	// SortBy is a map that contains the field name and the order.
+	SortBy map[string]string
+}
+
+type Result struct {
+	Total int32     `json:"total"`
+	Data  []Product `json:"data"`
+}
+
+type Searcher interface {
+	Search(ctx context.Context, term string, opt Options) (*Result, error)
+}
