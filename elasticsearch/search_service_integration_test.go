953525f3aa97e28b1621cb52db10a10e4a3d8ec9
Guilherme Santos <guilherme.santos@foodora.com>
2018-11-24 17:47:13 +0100

    Add test to filter by brand

--- elasticsearch/search_service_integration_test.go
+++ elasticsearch/search_service_integration_test.go
@@ -91,3 +91,57 @@ func TestSearch_FilterByProductTitle(t *testing.T) {
 		assert.Equal(t, expectedProduct.Stock, p.Stock)
 	}
 }
+
+func TestSearch_FilterByBrand(t *testing.T) {
+	ctx := context.Background()
+	esClient, cleanup := esClient(t)
+	defer cleanup()
+
+	searchSvc := elasticsearch.NewSearchService(esClient)
+	searchSvc.InitMapping(ctx)
+
+	loadESData(t, esClient, "products.json")
+
+	resp, err := searchSvc.Search(ctx, "t-shirt", gfgsearch.Options{
+		Page:    1,
+		PerPage: 5,
+		Filter: map[string]string{
+			"brand": "tommy hilfiger",
+		},
+	})
+
+	if !assert.NoError(t, err) {
+		t.FailNow()
+	}
+
+	assert.EqualValues(t, 2, resp.Total)
+	assert.Len(t, resp.Data, 2)
+
+	products := map[string]gfgsearch.Product{
+		"printed t-shirt": {
+			Title: "printed t-shirt",
+			Brand: "tommy hilfiger",
+			Price: 200,
+			Stock: 9,
+		},
+		"business shirt": {
+			Title: "business shirt",
+			Brand: "tommy hilfiger",
+			Price: 300,
+			Stock: 8,
+		},
+	}
+
+	for _, p := range resp.Data {
+		expectedProduct, ok := products[p.Title]
+		if !assert.True(t, ok) {
+			t.Logf("Product %q was not expected", p.Title)
+			t.FailNow()
+		}
+
+		assert.Equal(t, expectedProduct.Title, p.Title)
+		assert.Equal(t, expectedProduct.Brand, p.Brand)
+		assert.Equal(t, expectedProduct.Price, p.Price)
+		assert.Equal(t, expectedProduct.Stock, p.Stock)
+	}
+}
